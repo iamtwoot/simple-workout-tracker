010b03d1aae3739e20a6d3a4c2441a22bbb635ea
Twoot <inenquser@gmail.com>
2026-06-11 12:08:47 +0300

    Update imports in User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,4 @@
 from app.extensions import db
-import sqlalchemy as sql
 import sqlalchemy.orm as orm
 from datetime import datetime
 
@@ -7,12 +6,12 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    email: orm.Mapped[str] = orm.mapped_column(sql.String(64), index=True, unique=True)
-    username: orm.Mapped[str] = orm.mapped_column(sql.String(120), index=True, unique=True)
-    password_hash: orm.Mapped[str] = orm.mapped_column(sql.String(256))
+    email: orm.Mapped[str] = orm.mapped_column(db.String(64), index=True, unique=True)
+    username: orm.Mapped[str] = orm.mapped_column(db.String(120), index=True, unique=True)
+    password_hash: orm.Mapped[str] = orm.mapped_column(db.String(256))
 
-    created_at: orm.Mapped[datetime] = orm.mapped_column(sql.DateTime, default=datetime.now())
-    updated_at: orm.Mapped[datetime] = orm.mapped_column(sql.DateTime, default=datetime.now(), onupdate=datetime.now())
+    created_at: orm.Mapped[datetime] = orm.mapped_column(db.DateTime, default=datetime.now())
+    updated_at: orm.Mapped[datetime] = orm.mapped_column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
