c2917dcc6fa235afa763aeb05e17d6c3f8f6816d
Twoot <inenquser@gmail.com>
2026-06-11 11:27:02 +0300

    Configure database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from app.extensions import db
+
+# blueprints
 from app.routes.main import main_bp
+
 
 def create_app():
     app = Flask(__name__)
+
+    # temporarily SQLite
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout.db'
+
+    db.init_app(app)
 
     app.register_blueprint(main_bp)
 
