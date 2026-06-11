895f0b44c2e22c693fb527244091bb0a7b445078
Twoot <inenquser@gmail.com>
2026-06-11 14:15:35 +0300

    Create Workout model

---
+++ app/models/workout.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from app.extensions import db
+import sqlalchemy.orm as orm
+
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+
+    user_id: orm.Mapped[int] = orm.mapped_column(
+        db.ForeignKey("users.id"),
+        nullable=False,
+    )
+
+    name: orm.Mapped[str] = orm.mapped_column(db.String(120), nullable=False)
+
+    workout_date: orm.Mapped[datetime] = orm.mapped_column(
+        db.DateTime,
+        default=datetime.now,
+    )
+
+    notes: orm.Mapped[str] = orm.mapped_column(db.Text, nullable=True)
+
+    created_at: orm.Mapped[datetime] = orm.mapped_column(
+        db.DateTime,
+        default=datetime.now,
+    )
+
+    user = orm.relationship("User", back_populates="workouts")
