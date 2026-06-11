2bd8bfb4421baadecbe885a71c42d9c0a3b98c4a
Twoot <inenquser@gmail.com>
2026-06-11 15:47:02 +0300

    Remove 'main' route and small improvements

--- app/routes/workouts.py
+++ app/routes/workouts.py
@@ -4,11 +4,13 @@ from app.models.workout import Workout
 
 workouts_bp = Blueprint('workouts', __name__, url_prefix='/workouts')
 
-@workouts_bp.route('/', methods=['GET', 'POST'])
+@workouts_bp.route('/')
 def home():
-
     workouts = db.session.execute(db.select(Workout)).scalars().all()
     return render_template('workouts/list.html', workouts=workouts)
 
+@workouts_bp.route('/new')
+def new():
+    return render_template('workouts/new.html')
 
 
