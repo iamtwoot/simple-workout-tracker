af1bbf205c31e22d1b42d9a58c5d7747571860d1
Twoot <inenquser@gmail.com>
2026-06-15 12:52:28 +0300

    Create exercise update functionality

--- app/routes/exercises.py
+++ app/routes/exercises.py
@@ -18,3 +18,16 @@ def create_exercise(workout_id):
         return redirect(url_for('workouts.show_workout', workout_id=workout_id))
 
     return render_template('exercises/create_exercise.html', form=form)
+
+@exercises_bp.route('/workouts/<int:workout_id>/exercises/<int:exercise_id>/update', methods=['GET', 'POST'])
+def edit_exercise(workout_id, exercise_id):
+    exercise = db.get_or_404(Exercise, exercise_id)
+    form = ExerciseForm(obj=exercise)
+
+    if form.validate_on_submit():
+        exercise.name = form.name.data
+        db.session.add(exercise)
+        db.session.commit()
+        return redirect(url_for('workouts.show_workout', workout_id=workout_id))
+
+    return render_template('exercises/create_exercise.html', form=form)
