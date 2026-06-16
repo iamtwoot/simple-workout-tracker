9c51cb2b4babf170fd312d331aa8b0c2f8468d49
Twoot <inenquser@gmail.com>
2026-06-16 17:07:23 +0300

    Add block content to all templates

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return "Hello"
+    return render_template('index.html')
